4e2dd0a8944ade5ca9bb82cac5b33d801b5070ac
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-26 14:40:49 -0700

    Adjust package.json and server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-'use strict';
-
-var express = require('express');
-var routes = require('./app/routes/index.js');
+var express  = require('express');
 var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var routes   = require('./app/routes/index.js');
 
 var app = express();
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -16,16 +11,7 @@ app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+routes(app);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
