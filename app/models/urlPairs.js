e3b67c206120a84d2725ceb3b2b8890dc123b3ab
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-27 14:58:21 -0700

    Adjust UrlPair model to correctly create and validate original/short urls

--- app/models/urlPairs.js
+++ app/models/urlPairs.js
@@ -1,9 +1,40 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema;
 
-var UrlPair = new Schema({
-  original_url: String,
-  short_url:    String
+var IdHelper = new Schema({
+  _id: { type: String, required: true, unique: true },
+  seq: { type: Number, default: 0 }
+});
+
+var idHelper = mongoose.model('idHelper', IdHelper);
+
+idHelper.create({ _id: 'nanisore' });
+
+var UrlPair  = new Schema({
+  original_url: { type: String, unique: true, required: true },
+  short_url:    { type: Number, unique: true, required: true }
+});
+
+UrlPair.pre('validate', function(next) {
+  var doc = this;
+  if (doc.short_url) return next();
+
+  idHelper.findByIdAndUpdate(
+    { _id: 'nanisore' },
+    { $inc: { seq: 1 } },
+
+    function (err, idHelper) {
+      if (err) return next(err);
+      doc.short_url = idHelper.seq;
+
+      var url = doc.original_url;
+      
+      if (url.slice(0, 4) !== 'http')
+        url = 'http://' + url;
+
+      next();
+    }
+  );
 });
 
 module.exports = mongoose.model('UrlPair', UrlPair);
