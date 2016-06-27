27d7bf7cb738c0a432148306b2d311faf68ce128
Wolfgang C. Strack <wolfgangcstrack@gmail.com>
2016-06-26 20:26:08 -0700

    Set up routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,28 @@
 var path = process.cwd();
 
+// (not) the best url-matching regex
+var url_to_shorten = /^(https?:\/\/)?(.*?\.)*?.*?\..*?(\/.*?)?$/;
+
 module.exports = function (app) {
 	app.get('/', function (req, res) {
 		res.sendFile(path + '/public/index.html');
+	});
+
+	// there is no view or page for just '/new'
+	app.get('/new', function (req, res) {
+		res.redirect('/');
+	});
+
+	app.get(url_to_shorten, function(req, res) {
+		// TODO: call url-shortener controller
+		res.json({
+			"original_url": req.url,
+			"short_url":"https://chiisai-url.herokuapp.com/" + req.url.length
+		});
+	});
+
+	app.get('/:url_id', function(req, res) {
+		// TODO: call url redirect controller
+		res.redirect('/');
 	});
 };
